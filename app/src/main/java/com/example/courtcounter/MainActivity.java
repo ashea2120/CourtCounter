01af73776e4221b98cda11fdc33e154c0f977263
ashea2120 <ashea2120@yahoo.com>
2019-05-01 19:41:06 -0400

    Component layout all completed. Skeleton handlers for buttons.

---
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addThree(View view) {
+    }
+
+    public void addTwo(View view) {
+    }
+
+    public void freeThrow(View view) {
+    }
+}
